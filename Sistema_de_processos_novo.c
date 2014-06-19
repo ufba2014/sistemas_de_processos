9776db461bbd4fea963eeac942d68b5474a9689d
FelipeBaruch <lipepitanga@hotmail.com>
2014-06-19 20:49:32 -0300

    Update Sistema_de_processos_novo.c
    Só adicionado na estrutura a variavel "data" que é referente a data do cheque.

--- Sistema_de_processos_novo.c
+++ Sistema_de_processos_novo.c
@@ -4,23 +4,23 @@
 struct cli_Dados // ESTRUTURA DOS DADOS DOS CLIENTES
 {
   char nome[30],endereco[30],sup_Merc[30];
-  int ident, telefone, id;
+  int ident, telefone, id, data;
   float valor;
   struct cli_Dados *prox; // VAI APONTAR PARA O PROXIMO ELEMENTO DA ESTRUTURA
 };
 
 struct cli_Dados *gancho; //gancho funciona como uma transitividade para chamar o proximo item.
 
-void ini_fila() //FUNÇÂO QUE INICIA A FILA
+void ini_fila() //FUNï¿½ï¿½O QUE INICIA A FILA
 {
   struct cli_Dados *aux; //Ponteiro local do tipo cli_Dados
-  aux = (struct cli_Dados*) malloc(sizeof(struct cli_Dados)); //Aloca um espaço na memoria para a variavel aux
+  aux = (struct cli_Dados*) malloc(sizeof(struct cli_Dados)); //Aloca um espaï¿½o na memoria para a variavel aux
   aux->prox = (struct cli_Dados *)0; //Inicia o primeiro elemento da fila como valor nulo
-  gancho = aux; // aux é igual a primeiro elemnto da pilha.
+  gancho = aux; // aux ï¿½ igual a primeiro elemnto da pilha.
 }
 
 
-int main(void) //FUNÇÂO PRINCIPAL
+int main(void) //FUNï¿½ï¿½O PRINCIPAL
 {
   system("pause");
   return 0;
